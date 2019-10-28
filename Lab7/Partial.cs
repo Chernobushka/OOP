41db5ba2a62407adedc30f7e2ee75eb4459d0412
Chernobushka <chernobuhskaga1@gmail.com>
2019-10-29 01:04:08 +0300

    Lab 7 added

---
+++ Lab7/Partial.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    partial class Product
+    {
+        public override bool Equals(object obj)
+        {
+            Product product = (Product)obj;
+            return ((this.OrganizationName == product.OrganizationName) && (this.ProductName == product.ProductName));
+        }
+
+        public override int GetHashCode()
+        {
+            return this.OrganizationName.GetHashCode() + this.ProductName.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.OrganizationName + " " + this.ProductName;
+        }
+    }
+
+    enum list
+    {
+        one = 1,
+        two,
+        three,
+        nine = 9,
+        ten
+    }
+
+    public class Lab
+    {
+        public Base[] elems;
+        public int count = 0;
+        public int size;
+        list list = new list();
+
+        public Lab()
+        {
+            size = 100;
+            elems = new Base[100];
+        }
+
+        public Lab(int c)
+        {
+            if (c <= 0)
+                throw new WrongSize("Wrong size for lab!");
+            size = c;
+            elems = new Base[c];
+        }
+
+        public bool isFull()
+        {
+            return (count == size);
+        }
+
+        public bool isEmpty()
+        {
+            return (count == 0);
+        }
+
+        public void Add(Base el)
+        {
+            if (isFull())
+                throw new LabIsFull("Lab is full!");
+            elems[count++] = el;
+        }
+
+        public void Del(Base el)
+        {
+            int? num = null;
+            if (isEmpty())
+                throw new LabIsEmpty("Lab is empty!");
+            for (int i = 0; i < count; i++)
+            {
+                if (elems[i].Equals(el))
+                    num = i;
+            }
+            if (num == null)
+            {
+                throw new ElementDoesNotExist("There is no element!");
+            }
+            for (int i = (int)num; i < count - 1; i++)
+            {
+                elems[i] = elems[i + 1];
+            }
+            count--;
+        }
+
+        public void show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(elems[i].ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public abstract class Base
+    {
+        public struct Info
+        {
+            public int price;
+        }
+        public Info info = new Info();
+    }
+
+    public static class controller
+    {
+        public static void sort(Lab lab)
+        {
+            Base elem;
+            for (int i = 0; i < lab.count - 1; i++)
+            {
+                for (int j = 0; j < lab.count - i - 1; j++)
+                {
+                    if (lab.elems[j].info.price < lab.elems[j + 1].info.price)
+                    {
+                        elem = lab.elems[j];
+                        lab.elems[j] = lab.elems[j + 1];
+                        lab.elems[j + 1] = elem;
+                    }
+                }
+            }
+        }
+
+        public static void show(Lab lab)
+        {
+            string[] products = new string[6];
+            int[] count = new int[6];
+            int pos = 0;
+            bool b = false;
+            for (int j = 0; j < lab.count; j++)
+            {
+                for (int i = 0; i < pos; i++)
+                {
+                    if (products[i] == (lab.elems[j].GetType()).Name)
+                    {
+                        b = true;
+                        count[i]++;
+                        break;
+                    }
+                }
+                if (!b)
+                {
+                    products[pos] = lab.elems[j].GetType().Name;
+                    count[pos]++;
+                    pos++;
+                }
+                b = false;
+            }
+
+            Console.WriteLine("Lab elements:");
+            for (int i = 0; i < pos; i++)
+            {
+                Console.WriteLine($"{products[i]} - {count[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    interface IMyInterface
+    {
+        string MyFunc();
+    }
+
+    interface IAnotherInterface
+    {
+        string ToString();
+    }
+
+    partial class Product : Base, IAnotherInterface
+    {
+        public string OrganizationName { get; set; }
+        public string ProductName { get; set; }
+
+        public Product()
+        {
+            OrganizationName = "NoName organization";
+            ProductName = "NoName product";
+        }
+
+        public Product(string organization, string product, int price)
+        {
+            OrganizationName = organization;
+            ProductName = product;
+            info.price = price;
+        }
+    }
+
+    class Tech : Product, IAnotherInterface
+    {
+        public string ManufacturerName { get; set; }
+        public Tech() : base()
+        {
+            ManufacturerName = "NoName Manufacturer";
+        }
+
+        public Tech(string organization, string product, string manufacturer, int price) : base(organization, product, price)
+        {
+            ManufacturerName = manufacturer;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.ManufacturerName;
+        }
+    }
+
+    abstract class Computer : Tech, IAnotherInterface
+    {
+        public string OS { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.OS;
+        }
+
+        public Computer() : base()
+        {
+            OS = "NoName OS";
+        }
+
+        public Computer(string organization, string product, string manufacturer, string os, int price) : base(organization, product, manufacturer, price)
+        {
+            OS = os;
+        }
+
+        public abstract string MyFunc();
+    }
+
+    sealed class Printer : Tech, IAnotherInterface
+    {
+        public string IncType { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.IncType;
+        }
+
+        public Printer() : base()
+        {
+            IncType = "NoName Inc";
+        }
+
+        public Printer(string organization, string product, string manufacturer, string inc, int price) : base(organization, product, manufacturer, price)
+        {
+            IncType = inc;
+        }
+    }
+
+    class Scanner : Tech, IAnotherInterface
+    {
+        public string DocType { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.DocType;
+        }
+
+        public Scanner() : base()
+        {
+            DocType = "NoName DocType";
+        }
+
+        public Scanner(string organization, string product, string manufacturer, string doc, int price) : base(organization, product, manufacturer, price)
+        {
+            DocType = doc;
+        }
+    }
+
+    class Tablet : Computer, IMyInterface, IAnotherInterface
+    {
+        public string Resolution { get; set; }
+        public override string MyFunc()
+        {
+            return "Переопределение";
+        }
+        string IMyInterface.MyFunc()
+        {
+            return "Реализация метода интерфейса";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Resolution;
+        }
+    }
+
+    class Print
+    {
+        public static void IAmPrinting(IAnotherInterface obj)
+        {
+            Console.WriteLine(obj.ToString());
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("123", "213", 100);
+            Tech tech = new Tech("456", "789", "456789", 200);
+            Tablet tablet = new Tablet();
+            Lab lab = new Lab(6);
+            Scanner scanner = new Scanner("2223", "32131", "11123", "231", 22360);
+            tablet.info.price = 300;
+            IAnotherInterface[] arr = new IAnotherInterface[3];
+            arr[0] = product;
+            arr[1] = tech;
+            arr[2] = tablet;
+
+            try
+            {
+                Lab WrongLab = new Lab(100);
+                WrongLab.Del(tablet);
+                //arr[4].ToString();
+                lab.Add(product);
+                lab.Add(tech);
+                lab.Add(tablet);
+                lab.Add(new Scanner());
+                lab.Add(new Printer());
+                lab.Add(new Product());
+                //lab.Add(new Product());
+            
+
+                lab.show();
+                controller.show(lab);
+                controller.sort(lab);
+                lab.show();
+                lab.Del(tablet);
+                //lab.Del(scanner);
+                lab.show();
+            }
+            catch (LabIsFull ex)
+            {
+                Console.WriteLine("LabIsFull Exception");
+                Console.WriteLine(ex.Message);
+            }
+            catch (ElementDoesNotExist ex)
+            {
+                Console.WriteLine("ElementDoesNotExist Exception");
+                Console.WriteLine(ex.Message);
+            }
+            catch (LabIsEmpty ex)
+            {
+                Console.WriteLine("LabIsEmpty Exception");
+                Console.WriteLine(ex.Message);
+            }
+            catch (WrongSize ex)
+            {
+                Console.WriteLine("WrongSize Exception");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Another Exception");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
