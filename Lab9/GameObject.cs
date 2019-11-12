7796feab3a107d18fa1fcb450702492d6b9c580c
Chernobushka <chernobuhskaga1@gmail.com>
2019-11-12 19:56:41 +0300

    Final Lab9

--- Lab9/GameObject.cs
+++ Lab9/GameObject.cs
@@ -6,18 +6,22 @@ namespace ConsoleApp1
 {
     class GameObject
     {
+        static int count = 0;
         public int Health = 0;
         public int MaxHealth = 100;
+        public string Name;
         Random random = new Random();
 
         public GameObject()
         {
+            Name = "GameObject " + count++;
             Health = 100;
             MaxHealth = 100;
         }
 
         public GameObject(int hp)
         {
+            Name = "GameObject " + count++;
             Health = hp;
         }
         
@@ -26,9 +30,15 @@ namespace ConsoleApp1
             MaxHealth = max;
         }
 
+        ~GameObject()
+        {
+            count--;
+        }
+
         public void OnHeal(object sender, EventArgs e)
         {
             int heal = random.Next(10, 20);
+            Console.WriteLine($"{Name} health + {heal}");
             if (Health + heal >= MaxHealth)
                 Health = MaxHealth;
             else
@@ -37,10 +47,14 @@ namespace ConsoleApp1
 
         public void OnAttack(object sender, EventArgs e)
         {
-            Health -= random.Next(5, 30);
+            int damage = random.Next(5, 30);
+            Health -= damage;
+            Console.WriteLine($"{Name} health - {damage}");
             if (Health < 0)
+            {
                 Health = 0;
-            Console.WriteLine($"{this.ToString()} is dead!");
+                Console.WriteLine($"{Name} is dead!");
+            }
         }
     }
 }
--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -6,8 +7,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            GameObject obj1 = new GameObject();
-            Console.WriteLine(obj1.ToString());
+            void show(GameObject[] array)
+            {
+                Console.WriteLine("Health:");
+                foreach (GameObject x in array)
+                {
+                    Console.WriteLine($"{x.Name} health is {x.Health}");
+                }
+                Console.WriteLine();
+            }
+
+            StringBuilder str = new StringBuilder("S1 S2 A...");
+            Action<StringBuilder> action;
+            action = StringMethods.AddBAfterS;
+            action += StringMethods.DeleteSpaces;
+            action += StringMethods.AddSpacesAfterPoints;
+            action += StringMethods.DeleteCommas;
+            action += StringMethods.DeleteLetterA;
+
+            action(str);
+            Console.WriteLine($"{str}\n");
+
+            Game game = new Game();
+            GameObject[] arr = new GameObject[3];
+            arr[0] = new GameObject(100, 100);
+            arr[1] = new GameObject(100, 50);
+            arr[2] = new GameObject(3);
+
+            game.Attack += arr[0].OnAttack;
+            game.Attack += arr[2].OnAttack;
+            game.Heal += arr[1].OnHeal;
+            game.play();
+            show(arr);
+            game.Heal += arr[2].OnHeal;
+            game.play();
+            show(arr);
         }
     }
 }
