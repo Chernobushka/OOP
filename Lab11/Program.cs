f34b471c44ea36d0b3ecf7f5b4f849cbe6ae1fd9
Chernobushka <chernobuhskaga1@gmail.com>
2019-11-26 13:35:53 +0300

    Lab11 Final

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -17,20 +17,43 @@ namespace ConsoleApp1
             Console.Write("Введите n: ");
             n = Convert.ToInt32(Console.ReadLine());
             string[] arr = new string[12] { "December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November" };
-            IEnumerable<string> monthes = from x in arr
+            IEnumerable<string> months = from x in arr
                                           where x.Length < n
                                           select x;
-            IEnumerable<string> WinterAndSummer = arr.Take(3);
-            foreach(string x in monthes)
+            foreach(string x in months)
             {
                 Console.WriteLine(x);
             }
             Console.WriteLine();
+
+            Console.WriteLine("Зимние и летние месяцы:");
+            IEnumerable<string> WinterAndSummer = arr
+                .Take(3)
+                .Concat(arr.Take(9).Skip(6));
             foreach (string x in WinterAndSummer)
             {
                 Console.WriteLine(x);
             }
             Console.WriteLine();
+
+            Console.WriteLine("В алфавитном порядке:");
+            IEnumerable<string> AlphabetMonths = arr
+                .OrderBy(s => s);
+            foreach(string x in AlphabetMonths)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Содержащие букву \"u\" и с динной больше 4-х символов:");
+            IEnumerable<string> AnotherMonths = arr
+                .Where(s => (s.Contains('u')) && (s.Length > 4));
+            foreach (string x in AnotherMonths)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
             AbiturientList.Add(new Abiturient("Bad", "Student", marks[0]));
             AbiturientList.Add(new Abiturient("Normal", "Student", marks[1]));
             AbiturientList.Add(new Abiturient("Good", "Student", marks[2]));
@@ -59,16 +82,33 @@ namespace ConsoleApp1
             Console.WriteLine();
 
             Console.WriteLine("Упорядоченные по алфавиту:");
-            IEnumerable<Abiturient> AlphabetAbiturients = AbiturientList.OrderBy(s => s.LastName).ThenBy(s => s.FirstName);
+            IEnumerable<Abiturient> AlphabetAbiturients = AbiturientList
+                .OrderBy(s => s.LastName)
+                .ThenBy(s => s.FirstName);
             foreach (Abiturient x in AlphabetAbiturients)
             {
                 Console.WriteLine(x);
             }
             Console.WriteLine();
 
-            Console.WriteLine("3 с самой низкой успеваемостью");
-            IEnumerable<Abiturient> BadAvgAbiturinets = AbiturientList.OrderBy(s => s.getAvg()).Take(3);
+            Console.WriteLine("3 с самой низкой успеваемостью:");
+            IEnumerable<Abiturient> BadAvgAbiturinets = AbiturientList
+                .OrderBy(s => s.getAvg())
+                .Take(3);
             foreach (Abiturient x in BadAvgAbiturinets)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Свой запрос:");
+            IEnumerable<string> abiturients = AbiturientList
+                .OrderBy(s => s.getAvg())
+                .Where(s => s.getMin() > 3)
+                .Skip(1)
+                .Select(s => s.FirstName + " " + s.LastName)
+                .Concat(AlphabetAbiturients.Select(s => s.FirstName + " " + s.LastName));
+            foreach(string x in abiturients)
             {
                 Console.WriteLine(x);
             }
